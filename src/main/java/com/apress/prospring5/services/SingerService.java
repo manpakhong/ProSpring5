cad67eeec36ae3f0362b93b015663b35c2de1798
manpakhong <manpakhong@192.168.1.133>
2020-03-06 16:45:09 +0800

    20200306_1544-Runnable version

--- src/main/java/com/apress/prospring5/services/SingerService.java
+++ src/main/java/com/apress/prospring5/services/SingerService.java
@@ -1,13 +1,14 @@
 package com.apress.prospring5.services;
 
+import java.util.List;
+
 import com.apress.prospring5.entities.Singer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface SingerService {
     List<Singer> findAll();
     Singer findById(Long id);
     Singer save(Singer singer);
+    Page<Singer> findAllByPage(Pageable pageable);
 }
--- src/main/java/com/apress/prospring5/services/SingerServiceImpl.java
+++ src/main/java/com/apress/prospring5/services/SingerServiceImpl.java
@@ -1,25 +1,30 @@
 package com.apress.prospring5.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.apress.prospring5.entities.Singer;
 import com.apress.prospring5.repos.SingerRepository;
 import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.util.List;
+
+@Transactional
+@Service("singerService")
 public class SingerServiceImpl implements SingerService {
 
 	private SingerRepository singerRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Singer> findAll() {
 		return Lists.newArrayList(singerRepository.findAll());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Singer findById(Long id) {
 		return singerRepository.findById(id).get();
 	}
@@ -34,4 +39,9 @@ public class SingerServiceImpl implements SingerService {
 		this.singerRepository = singerRepository;
 	}
 
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Singer> findAllByPage(Pageable pageable) {
+		return singerRepository.findAll(pageable);
+	}
 }
